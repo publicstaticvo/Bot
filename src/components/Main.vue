90add581412d95ce06ac00976989a6c4b4c00c59
余天枢 <2017011406@secoder.net>
2019-11-28 00:28:57 +0800

    0

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header></el-header>
+      <el-main></el-main>
+      <el-footer></el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main',
+  }
+</script>
+
+<style>
+</style>
